c76b32e5eeb8901059130a83c5879f8a58d396d1
KonradJadwizyc <konradjadwizyc@gmail.com>
2020-05-22 00:00:40 +0200

    dodanie zadania 4 i 5 zestaw1

--- zad_5.py
+++ zad_5.py
@@ -5,35 +5,56 @@ import matplotlib.pyplot as plt
 # read dataset
 df = pd.read_csv("survey_results_schema.csv", encoding= "UTF-8")
 df_2 = pd.read_csv("survey_results_public.csv",encoding = "UTF-8",
-                   usecols=["Respondent","YearsCodePro","WorkWeekHrs"],
+                   usecols=["Respondent","Gender","WorkWeekHrs","Age","OrgSize"],
                    index_col= "Respondent")
 
 df_2 = pd.DataFrame(df_2)
 
 print(df_2.shape)
 # delete Nan value
-df_2_delete_nan = df_2.dropna()
+df_2 = df_2.dropna()
 
-print(df_2_delete_nan)
+print(df_2)
 
-print(df_2_delete_nan.shape)
-print(df_2_delete_nan.dtypes)
-print(df_2_delete_nan.info())
-# check column value and print unique value
-columns_values = df_2_delete_nan[["YearsCodePro"]].values
-unique_values = np.unique(columns_values)
+print(df_2.shape)
+print(df_2.dtypes)
+print(df_2.info())
+
+df_2["WorkWeekHrs"] = df_2["WorkWeekHrs"] / 40
+column_values = df_2[["OrgSize"]].values
+unique_values = np.unique(column_values)
 print(unique_values)
-# replace non numeric value to numeric
-df_2_delete_nan = df_2_delete_nan.replace(to_replace={"Less than 1 year":"0"})
+# replace value
+df_2 = df_2.replace(to_replace={"1,000 to 4,999 employees":"1,000 - 4,999"})
+df_2 = df_2.replace(to_replace={"10 to 19 employees":"10 - 19"})
+df_2 = df_2.replace(to_replace={"10,000 or more employees":"10,000 - more"})
+df_2 = df_2.replace(to_replace={"2 to 9 employees":"2 - 9"})
+df_2 = df_2.replace(to_replace={"100 to 499 employees":"100 - 499"})
+df_2 = df_2.replace(to_replace={"20 to 99 employees":"20 - 99"})
+df_2 = df_2.replace(to_replace={"5,000 to 9,999 employees":"5,000 - 9,999"})
+df_2 = df_2.replace(to_replace={"500 to 999 employees":"500 - 999"})
+
+column_values = df_2[["OrgSize"]].values
+unique_values = np.unique(column_values)
+print(unique_values)
 # check col value
-columns_values = df_2_delete_nan[["YearsCodePro"]].values
-unique_values = np.unique(columns_values)
-print(unique_values)
 # value to int64
-df_2_delete_nan = df_2_delete_nan.astype("int64", copy=False)
-print(df_2_delete_nan.dtypes)
+print(df_2.dtypes)
 # prepare plot in matplitlib
-plt.plot(df_2_delete_nan["YearsCodePro"], df_2_delete_nan["WorkWeekHrs"], "ro", markersize=0.3)
-plt.xlabel("YearsCodePro")
-plt.ylabel("WorkWeekHrs")
+# first plot
+#gdf_2 = df_2.groupby("Gender")["WorkWeekHrs"].plot.barh()
+
+plt.plot(df_2["WorkWeekHrs"],df_2["OrgSize"],"ro" ,markersize=0.5)
+plt.xlabel("Work dayli")
+plt.ylabel("Organization Size")
+plt.show()
+
+# second plot
+val = [df_2.loc[df_2["Gender"] == "Man", "Age"],
+        df_2.loc[df_2["Gender"] == "Woman", "Age"]]
+fig = plt.figure(1,figsize=(9,6))
+ax = fig.add_subplot(111)
+bp = ax.boxplot(val, labels = list("MK"))
+plt.title("Decomposition Age by Gender")
+plt.ylabel("Age")
 plt.show()
